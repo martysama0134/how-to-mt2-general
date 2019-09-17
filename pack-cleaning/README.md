014e6683d13bc04cdf3ec7cdd22318f8a66ceec6
Marty Sama <martysama0134@gmail.com>
2019-09-17 12:51:32 +0200

    pack_index_make.py

--- pack-cleaning/README.md
+++ pack-cleaning/README.md
@@ -4,3 +4,4 @@
 - `pack_merge_files.py` will merge all the (unpacked) packs into a single `d_/` folder
 - `pack_generate.py` will split a `d_/` folder into new ready-to-pack packs (all maps in `maps`, and no useless `patch` packs)
 - `pack_delete_empty_folders.sh` will remove all the empty folders
+- `pack_index_make.py` will generate a new Index from the (unpacked) packs
---
+++ pack-cleaning/pack_index_make.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python2.7
+#### @martysama0134 pack-cleaning scripts ####
+import os
+
+outp_idx = "newIndex"
+
+def walklevel(some_dir, level=1):
+	some_dir = some_dir.rstrip(os.path.sep)
+	assert os.path.isdir(some_dir)
+	num_sep = some_dir.count(os.path.sep)
+	for root, dirs, files in os.walk(some_dir):
+		yield root, dirs, files
+		num_sep_this = root.count(os.path.sep)
+		if num_sep + level <= num_sep_this:
+			del dirs[:]
+
+def get_folder_list(folder="."):
+	dir_list = [i[0] for i in walklevel(folder)]
+	folder_prefix = os.path.join(folder,"")
+	dirs = []
+	for dir in dir_list:
+		fold_name = dir[len(folder_prefix):]
+		if fold_name=="":
+			continue
+		if fold_name=="d_": # skip
+			continue
+		dirs.append(fold_name)
+	return dirs
+
+def process_list(pack_list):
+	with open(outp_idx, "w") as f1:
+		f1.write("PACK\n")
+		f1.write("".join([("*\n%s\n" % i) for i in pack_list])) #old format
+		# f1.write("\n".join(pack_list)) #new offy format
+
+if __name__ == "__main__":
+	process_list(get_folder_list())
+
