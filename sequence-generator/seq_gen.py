a0c1d69ab61be05ca26fc0a441613765c19175f4
Marty Sama <martysama0134@users.noreply.github.com>
2020-05-19 05:37:23 +0200

    Update seq_gen.py
    py3 support

--- sequence-generator/seq_gen.py
+++ sequence-generator/seq_gen.py
@@ -14,13 +14,13 @@ def genKey():
 	return randrange(0x0,0xfe)
 
 def genLine():
-	return " ".join(["0x%x," % genKey() for i in xrange(16)])
+	return " ".join(["0x%x," % genKey() for i in range(16)])
 
 if __name__ == "__main__":
 	with open("seq_gen.txt", "w") as seqFile:
 		seqFile_write = seqFile.write
 		seqFile_write("{\n")
-		for i in xrange(2048):
+		for i in range(2048):
 			seqFile_write("\t%s\n" % genLine())
 		seqFile_write("}\n")
 #
