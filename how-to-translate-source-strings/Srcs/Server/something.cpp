e6dd90a5ec8dfa13abbeb9b4de8f72c7e7f7bdc7
Marty Sama <martysama0134@users.noreply.github.com>
2023-05-30 16:53:18 +0200

    example

---
+++ how-to-translate-source-strings/Srcs/Server/something.cpp
@@ -0,0 +1,10 @@
+
+						ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("용혼석 추출에 성공하였습니다."));
+
+						ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("용혼석 추출에 실패하여 %s를 얻었습니다."));
+
+						ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("용혼석 추출에 실패하였습니다."));
+
+						ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("제조에 실패하였습니다."));
+
+						ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("아이템이 나올 확률"));
